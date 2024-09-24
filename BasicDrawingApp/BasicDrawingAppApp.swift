25241c0048300919549ebadbc23c7ddd7f128e59
Sandi Junker <sandi.junker@outlook.com>
2024-09-24 17:10:04 -0500

    Initial Commit

---
+++ BasicDrawingApp/BasicDrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicDrawingAppApp.swift
+//  BasicDrawingApp
+//
+//  Created by Sandi Junker on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicDrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
