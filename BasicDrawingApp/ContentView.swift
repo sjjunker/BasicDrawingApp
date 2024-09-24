009f06c292c1a0d48bbb6602a56737656751c0b9
Sandi Junker <sandi.junker@outlook.com>
2024-09-24 17:30:51 -0500

    Initial commit

--- BasicDrawingApp/ContentView.swift
+++ BasicDrawingApp/ContentView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var paths: [Path] = []
+    @State private var currentPath = Path()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Canvas { context, size in
+                for path in paths {
+                    context.stroke(path, with: .color(.black), lineWidth: 2)
+                }
+                context.stroke(currentPath, with: .color(.black), lineWidth: 2)
+            }
+            .frame(width: 300, height: 300)
+            .border(Color.blue)
+            .gesture(DragGesture()
+                .onChanged{value in currentPath.addLine(to: value.location)
+                }
+                .onEnded{value in paths.append(currentPath)
+                    currentPath = Path()
+                }
+            )
         }
         .padding()
     }
